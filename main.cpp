99930a1e75fc7734bd775bae94ed59cbd6797cba
davidedumg-hub <davidedumg@gmail.com>
2025-09-19 22:28:27 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ struct Point
 };
 
 // Función para calcular la distancia entre dos puntos
-// ✅ Pasar por referencia constante para eficiencia
+// Pasar por referencia constante para eficiencia
 double calcularDistancia(const Point &p1, const Point &p2)
 {
     return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
